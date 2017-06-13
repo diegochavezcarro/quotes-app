bb877a8d2f286241f34158e7529723beb52d8681
Diego Chavez <diegochavezcarro@gmail.com>
2017-06-13 17:33:23 -0300

    ion list

--- src/pages/library/library.ts
+++ src/pages/library/library.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import {Quote} from '../../data/quote.interface'
+import quotes from '../../data/quotes'
 /**
  * Generated class for the LibraryPage page.
  *
@@ -15,6 +16,12 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 export class LibraryPage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  quoteCollection: {category:string, quotes: Quote[], icon:string}[];
+
+  ngOnInit(){
+    this.quoteCollection=quotes;
   }
 
   ionViewDidLoad() {
