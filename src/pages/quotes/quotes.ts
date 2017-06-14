6f357aa08b6c7938eb1b4230ad1ffe0a2edcffbb
Diego Chavez <diegochavezcarro@gmail.com>
2017-06-14 09:56:07 -0300

    section 3 lecture 52

--- src/pages/quotes/quotes.ts
+++ src/pages/quotes/quotes.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { Quote } from '../../data/quote.interface'
+import { Quote } from '../../data/quote.interface';
+import {AlertController} from 'ionic-angular';
 @IonicPage()
 @Component({
   selector: 'page-quotes',
@@ -8,11 +9,36 @@ import { Quote } from '../../data/quote.interface'
 })
 export class QuotesPage {
   quoteGroup: { category: string, quotes: Quote[]; icon: string };
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, 
+    public navParams: NavParams, private alertCtrl:AlertController) {
   }
 
   ngOnInit() {
     this.quoteGroup = this.navParams.data;
+  }
+  onAddToFavorite(selectedQuote:Quote){
+    const alert = this.alertCtrl.create({
+      title: 'Add Quote',
+      subTitle: 'Are you sure',
+      message: 'Are you sure you want to add the quote',
+      //buttons: ['Ok']
+      buttons:[
+        {
+          text: 'Yes, go ahead',
+          handler: ()=> {
+            console.log('Ok');
+          }
+        },
+        {
+          text: 'No, I changed my mind',
+          role: 'cancel', //si clickeamos afuera va a llamar tmb al handler
+          handler: ()=> {
+            console.log('Canceled!');
+          }
+        }
+      ]
+    });
+    alert.present();
   }
 /*
 //use elvis operator (?) in template to use this approach
--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -6,8 +6,8 @@ import {LibraryPage} from '../library/library';
     selector: 'page-tabs',
     template: `
         <ion-tabs selectedIndex="0">
-            <ion-tab [root]="favoritesPage" tabTitle="Favorites" tbIcon="star"> </ion-tab>
-            <ion-tab [root]="libraryPage" tabTitle="Library" tbIcon="book"> </ion-tab>
+            <ion-tab [root]="favoritesPage" tabTitle="Favorites" tabIcon="star"> </ion-tab>
+            <ion-tab [root]="libraryPage" tabTitle="Library" tabIcon="book"> </ion-tab>
         </ion-tabs>
     `
 })
