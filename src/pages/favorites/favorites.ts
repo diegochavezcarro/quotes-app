37fe4107393a02532137a9c5a436bfbc655981c1
Diego Chavez <diegochavezcarro@gmail.com>
2017-06-15 11:20:00 -0300

    section 3 lecture 64

--- src/pages/favorites/favorites.ts
+++ src/pages/favorites/favorites.ts
@@ -1,7 +1,9 @@
+import { QuotePage } from './../quote/quote';
 import { Quote } from './../../data/quote.interface';
 import { QuotesService } from './../../services/quotes';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {ModalController} from 'ionic-angular';
 
 @IonicPage()
 @Component({
@@ -12,11 +14,33 @@ export class FavoritesPage {
   quotes: Quote[];
   constructor(public navCtrl: NavController,
     public navParams: NavParams,
-    private quotesService: QuotesService) {
+    private quotesService: QuotesService,
+    private modalCtrl: ModalController) {
   }
 
   ionViewWillEnter() {
     this.quotes=this.quotesService.getFavoriteQuotes();
+  }
+
+  onViewQuote(quote:Quote){
+    const modal = this.modalCtrl.create(QuotePage,quote);
+    modal.present();
+    modal.onDidDismiss((remove:boolean)=>{
+      //si se da click en no, da false. Click afuera da null.
+      //click en sida true.
+      //console.log(remove);
+      if(remove){
+        this.quotesService.removeQuoteFromFavorites(quote);
+        this.quotes=this.quotesService.getFavoriteQuotes();
+        //alternativa si se quiere borrar sin recargar todo:
+        //const position= this.quotes.indexOf(quote);
+        //this.quotes.splice(position,1);
+      }
+    });
+    //no existen estos hooks en nuevas versiones
+    /*modal.willLeave.subscribe(
+      (remove:boolean)=>console.log(remove)
+    );*/
   }
 
 }
--- src/pages/quote/quote.ts
+++ src/pages/quote/quote.ts
@@ -1,24 +1,26 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
 
-/**
- * Generated class for the QuotePage page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
 @IonicPage()
 @Component({
   selector: 'page-quote',
   templateUrl: 'quote.html',
 })
 export class QuotePage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  person:string;
+  text:string;
+  constructor(public navCtrl: NavController, 
+    public navParams: NavParams,
+    private viewCtrl: ViewController) {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad QuotePage');
+    this.person=this.navParams.get('person');
+    this.text=this.navParams.get('text');
+  }
+//default false, si no se le pasa ningun argumento
+  onClose(remove=false){
+    this.viewCtrl.dismiss(remove);
   }
 
 }
